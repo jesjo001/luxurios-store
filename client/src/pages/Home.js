2dec5c07de810a87487f088ecb796949b4874fac
Joshua <31279142+jesjo001@users.noreply.github.com>
2022-03-18 21:54:04 +0100

    Updated product list

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,63 @@
 import React from 'react'
+import styled from 'styled-components'
+import ProductCard from './Cards/ProductCard'
+import shoe from '../uploads/product-1.jpg';
 
+const data = [
+    {
+        img: shoe,
+        title: "Sport Sneakers",
+        description: "asf asdfa asdas asdf asd asd asd asd asd asd asd asd ",
+        price: "$123"
+    },
+    {
+        img: shoe,
+        title: "Sport Sneakers",
+        description: "asf asdfa asdas asdf asd asd asd asd asd asd asd asd ",
+        price: "$123"
+    },
+    {
+        img: shoe,
+        title: "Sport Sneakers",
+        description: "asf asdfa asdas asdf asd asd asd asd asd asd asd asd ",
+        price: "$123"
+    },
+    {
+        img: shoe,
+        title: "Sport Sneakers",
+        description: "asf asdfa asdas asdf asd asd asd asd asd asd asd asd ",
+        price: "$123"
+    }
+]
 const Home = () => {
     return (
         <div>
-            <p>Home</p>
+            <HorizontalSection>
+                {
+                    data.map((item, index) => {
+                        return (
+                            <ProductCard 
+                                img={item.shoe} 
+                                title={item.title}
+                                description={item.description}
+                                price={item.price}
+                            />
+               
+                        )
+                    })
+                }
+                 <ProductCard />
+                <ProductCard />
+            </HorizontalSection>
         </div>
     )
 }
 
 export default Home
+
+const HorizontalSection = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`
